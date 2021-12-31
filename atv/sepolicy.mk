73edd7003a0e4350fa9231f73bca19edef6145c7
Bartłomiej Rudecki <ejbtrd@gmail.com>
2021-12-31 21:49:03 +0100

    sepolicy: Adapt for ProjectStreak

--- atv/sepolicy.mk
+++ atv/sepolicy.mk
@@ -1,12 +1,12 @@
 #
 # This policy configuration will be used by atv products that
-# inherit from Lineage
+# inherit from Streak
 #
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/atv/vendor
+    device/streak/sepolicy/atv/vendor
 endif
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/atv/private
+   device/streak/sepolicy/atv/private
