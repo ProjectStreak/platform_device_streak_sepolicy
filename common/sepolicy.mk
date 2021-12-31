9d23c4fa35676e112d0a0bbfa67a6e62cce01d82
Bartłomiej Rudecki <ejbtrd@gmail.com>
2021-12-31 21:49:03 +0100

    sepolicy: Adapt for ProjectStreak

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -16,20 +16,20 @@ endif
 endif
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/arrow/sepolicy/common/public
+    device/streak/sepolicy/common/public
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/arrow/sepolicy/common/private
+    device/streak/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/arrow/sepolicy/common/dynamic \
-    device/arrow/sepolicy/common/system
+    device/streak/sepolicy/common/dynamic \
+    device/streak/sepolicy/common/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/arrow/sepolicy/common/dynamic \
-    device/arrow/sepolicy/common/vendor
+    device/streak/sepolicy/common/dynamic \
+    device/streak/sepolicy/common/vendor
 endif
 
 # Selectively include legacy rules defined by the products
--include device/arrow/sepolicy/legacy-common/sepolicy.mk
+-include device/streak/sepolicy/legacy-common/sepolicy.mk
